7a0f37c1bb87ead2fe3dec820ce0476c514ced9c
Ronen Franzman <ronen.franzman@gmail.com>
2025-08-24 18:35:28 -0300

    Complete project

---
+++ script.js
@@ -0,0 +1,103 @@
+alert("Welcome to rock paper scissors");
+playGame();
+
+function getComputerChoice() {
+    //Rock = 0, Paper = 1, Scissors = 2
+    let choice = Math.floor(Math.random() * 3);
+
+    switch (choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Please type: Rock, Paper, or Scissors");
+
+    // Lowercase human's choice and return
+    return choice.toLowerCase();
+}
+
+// Returns -1 on tie or invalid round, 0 on computer win, 1 on human win
+function playRound(human, computer) {
+    // Bad input
+    if(human != "rock" && human != "paper" && human != "scissors"){
+        alert("Invalid choice, continuing to next round.");
+        return -1;
+    // Tie
+    } else if (human == computer) {
+        alert("Tie, you both chose " + human + "!");
+        return -1;
+    } else if (human == "rock") {
+        if (computer == "paper") {
+            alert("You lose! Paper beats Rock.");
+            return 0;
+        } else {
+            alert("You win! Rock beats Scissors.");
+            return 1;
+        }
+    } else if (human == "paper") {
+        if (computer == "scissors") {
+            alert("You lose! Scissors beats Paper.");
+            return 0;
+        } else {
+            alert("You win! Paper beats Rock.");
+            return 1;
+        }
+    } else {
+        if (computer == "rock") {
+                alert("You lose! Rock beats Scisosrs.");
+            return 0;
+        } else {
+            alert("You win! Scissors beats Paper.");
+            return 1;
+        }
+    }
+}
+
+function playGame() {
+
+    // Keep track of scores
+    let humanScore = 0;
+    let computerScore = 0;
+    // Keep track of rounds
+    let round = 0;
+    // Keep track of game state
+    let keepGoing = true;
+
+    // Loop rounds
+    while(keepGoing) {
+        // Display round counter
+        alert(`Round ${++round}`);
+
+        // Get choices
+        const computer = getComputerChoice();
+        const human = getHumanChoice();
+
+        // Get round winner
+        let roundWinner = playRound(human, computer);
+
+        // Update points
+        if (roundWinner == 0) {
+            computerScore++;
+        } else if (roundWinner == 1) {
+            humanScore++;
+        }
+
+        if(computerScore == 3 || humanScore == 3) {
+            keepGoing = false;
+        }
+
+        alert("Scores are human: " + humanScore + " and computer: " + computerScore);
+    }      
+
+    if (humanScore == 3) {
+        alert("Congratulations! You win!");
+    } else {
+        alert("Sorry! You lose!");
+    }
+}
